948cd0958814173baa59507e29a914f18449e377
bijay89 <bijaykumarbikibhai@gmail.com>
2024-06-18 19:19:38 +0530

    comment for myprodu-branch

--- src/simpleName11/SimpleName.java
+++ src/simpleName11/SimpleName.java
@@ -3,7 +3,11 @@ package simpleName11;
 public class SimpleName {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//simple name
+		
+		
+		
+		System.out.println("hii this is bijay");
 
 	}
 
